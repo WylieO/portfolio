62c43c3bda196b292b65cd5f407f1333e8ba0079
Wylie Oliver <wylieo14@gmail.com>
2022-05-06 09:26:38 -0500

    Updates

---
+++ precache-manifest.6d38e6aeddc772e81ce460b4a1c707b3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6543385328405f44f87619b0800c08b1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dc2e7e96e260e5b0de95",
+    "url": "/portfolio/static/css/main.990b79c0.chunk.css"
+  },
+  {
+    "revision": "2fb715e002b586b923ec",
+    "url": "/portfolio/static/js/2.4fb1e589.chunk.js"
+  },
+  {
+    "revision": "9e80ed6ee19e2ff5e581d429458bb084",
+    "url": "/portfolio/static/js/2.4fb1e589.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc2e7e96e260e5b0de95",
+    "url": "/portfolio/static/js/main.75d1c716.chunk.js"
+  },
+  {
+    "revision": "7c65e6a7cb1ecedf1b8e",
+    "url": "/portfolio/static/js/runtime-main.bf310185.js"
+  },
+  {
+    "revision": "16c55b1a9bb6bcc68bce1f8a0c576aed",
+    "url": "/portfolio/static/media/cup_holder.16c55b1a.png"
+  },
+  {
+    "revision": "6d32619a9ae65fa1ff5332133443762f",
+    "url": "/portfolio/static/media/headboard.6d32619a.png"
+  },
+  {
+    "revision": "dbd7c51b13a3bb4f3fef4b3c1d7779d8",
+    "url": "/portfolio/static/media/key_holder.dbd7c51b.png"
+  },
+  {
+    "revision": "ce82703a4c0d1d818b3e6c9b220c958c",
+    "url": "/portfolio/static/media/knife_holder.ce82703a.png"
+  },
+  {
+    "revision": "898b804975a80de2a6c59e503ade511b",
+    "url": "/portfolio/static/media/linkedin-icon.898b8049.png"
+  }
+]);
